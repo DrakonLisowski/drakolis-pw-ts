4946830ecff36d7875c8150216b2d4ca0cf6ff75
mika.zimina <mika.zimina@centro.team>
2019-08-05 13:22:55 +0300

    Fix something

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import compression from 'compression';  // compresses requests
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import lusca from 'lusca';
-import dotenv from 'dotenv';
 import mongo from 'connect-mongo';
 import flash from 'express-flash';
 import path from 'path';
@@ -38,7 +37,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true} ).then(
 });
 
 // Express configuration
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 app.use(compression());
@@ -80,40 +79,5 @@ app.use((req, res, next) => {
 app.use(
     express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }),
 );
-
-/**
- * Primary app routes.
- */
-app.get('/', homeController.index);
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
-app.get('/logout', userController.logout);
-app.get('/forgot', userController.getForgot);
-app.post('/forgot', userController.postForgot);
-app.get('/reset/:token', userController.getReset);
-app.post('/reset/:token', userController.postReset);
-app.get('/signup', userController.getSignup);
-app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
-app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
-app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
-app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
-
-/**
- * API examples routes.
- */
-app.get('/api', apiController.getApi);
-app.get('/api/facebook', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
-
-/**
- * OAuth authentication routes. (Sign in)
- */
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
-    res.redirect(req.session.returnTo || '/');
-});
 
 export default app;
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,5 +1,6 @@
 import * as wins from 'winston';
-import { createLogger, LoggerOptions, transports, format } from 'winston';
+// tslint:disable-next-line: no-duplicate-imports
+import { createLogger, LoggerOptions, transports } from 'winston';
 import winstonDailyRotateFile from 'winston-daily-rotate-file';
 
 const {
@@ -24,7 +25,7 @@ export default (labels: string) => {
       myFormat,
     ),
     transports: [
-      new DailyRotator({
+      new winstonDailyRotateFile({
         filename: 'log-%DATE%.log',
         dirname: './logs',
         datePattern: 'YYYY-MM-DD',
