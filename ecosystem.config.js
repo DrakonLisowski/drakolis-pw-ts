38f4d18680c161dd7880cf861c968d2c4432a6a0
mika.zimina <mika.zimina@centro.team>
2019-09-13 09:59:50 +0300

    Config for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -50,6 +50,22 @@ module.exports = {
         NODE_ENV: 'production'
       },
       kill_timeout : 3000
+    },
+    {
+      name: '[Drakolis.PW] BotTG Channel Manager',
+      script: 'ts-node',
+      args: './src/runner bot-tg-channelmanager',
+      instances: 1,
+      autorestart: true,
+      watch_delay: 5000,
+      watch: ['src'],
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      kill_timeout : 3000
     }
   ],
 /*
