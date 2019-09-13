2a0831ae0e383760229fc719367ae0e4bf18cd66
mika.zimina <mika.zimina@centro.team>
2019-09-13 13:17:53 +0300

    Remove test

--- src/scripts/migrate.ts
+++ src/scripts/migrate.ts
@@ -10,7 +10,6 @@ const runScript = async() => {
   log.info('Connecting to DB...');
   let typeorm;
   try {
-    log.info(process.env.NODE_ENV);
     typeorm = await createConnection(dbSettingsBuilder(log));
     log.info('DB connected!');
     await typeorm.runMigrations();
