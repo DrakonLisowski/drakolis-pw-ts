8bdf5ebf2ef436b5b826beb29523b130f99cde0c
mika.zimina <mika.zimina@centro.team>
2019-09-18 11:32:04 +0300

    Commands now work more safely

--- src/apps/websocket/commandWrapper.ts
+++ src/apps/websocket/commandWrapper.ts
@@ -5,12 +5,13 @@ import { Utils, JSONRPCRequest, JSONRPCError } from 'jayson';
 export default function commandWrapper(
   socket: socketIo.Socket,
   command: Command,
-): (data: JSONRPCRequest) => void {
-  return (data: JSONRPCRequest) => {
-    if (data.method === command.getName()) {
-      command.getFunction()(data.params)
-        .then((result: any) => socket.send(Utils.response(null, result, data.id)))
-        .catch((error: JSONRPCError) => socket.send(Utils.response(error, null, data.id)));
-    }
+) {
+  const handler = (data: JSONRPCRequest) => {
+    command.getFunction()(data.params)
+      .then((result: any) => socket.send(Utils.response(null, result, data.id)))
+      .catch((error: JSONRPCError) => socket.send(Utils.response(error, null, data.id)));
+  };
+  return {
+    [command.getName()]: handler,
   };
 }
