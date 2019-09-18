9eac563e071278d4cd5e419645012d0d094c4712
mika.zimina <mika.zimina@centro.team>
2019-09-18 10:55:24 +0300

    Requests are now tested

--- src/commands/test/DrawAPenis.ts
+++ src/commands/test/DrawAPenis.ts
@@ -1,6 +1,8 @@
 import { Command } from '../Command';
+import { AuthorizationTokenRequest } from '../../requests/AuthorizationTokenRequest';
 
 export default new Command(
+  AuthorizationTokenRequest,
   'drawAPenis',
   async () => '8===D',
   true,
