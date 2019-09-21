8d2568498e6c8748fafe5df29f2afb7268eee582
mika.zimina <mika.zimina@centro.team>
2019-09-21 12:25:15 +0300

    Context v1.0

--- src/services/context/index.ts
+++ src/services/context/index.ts
@@ -3,36 +3,66 @@ import { Service } from '../ServiceDecorator';
 @Service()
 export default class ContextService {
 
+  private static normalizeStringArguments(args: string[]|string) {
+    return Array.isArray(args) ? args : [args];
+  }
+
   private contextMap: WeakMap<object, string[]> = new WeakMap();
 
+  /**
+   * Adds context to root element
+   * @param context context to add
+   */
   public addRootContext(context: string[]|string) {
-    this.addContext(null, context);
+    this.addContext(this, context);
     return this;
   }
 
+  /**
+   * Gets context of a root element
+   */
   public getRootContext(): string[] {
-    if (this.contextMap.has(null)) {
-      return this.contextMap.get(null);
+    if (this.contextMap.has(this)) {
+      return this.contextMap.get(this);
     }
     return [];
   }
 
+  /**
+   * Creates new context for an object, adding to context of a parent
+   * @param obj object to create context for
+   * @param parent parent to extend, null for root element
+   * @param newContext new values added to parent's context
+   */
   public addSubContext(obj: object, parent: object = null, newContext: string[]|string = []) {
-    const parentContext = this.getContext(parent);
-    this.addContext(obj, [...parentContext, ...newContext]);
+    const context = ContextService.normalizeStringArguments(newContext);
+
+    const parentContext = this.getContext(parent || this);
+    this.addContext(obj, [...parentContext, ...context]);
   }
 
+  /**
+   * Gets context of an object
+   * @param obj object to get context of
+   */
   public getContext(obj: object): string[] {
     if (!this.contextMap.has(obj)) {
-      return [...this.getRootContext()];
+      return [];
     }
-    return [...this.getRootContext(), ...this.contextMap.get(obj)];
+    return [...this.contextMap.get(obj)];
   }
 
-  protected addContext(obj: object, newContext: string[]|string) {
-    let context = Array.isArray(newContext) ? newContext : [newContext];
+  /**
+   * Creates or adds context to an object.
+   * This should probably be used only in special situations,
+   * because the context of this object wont extend root context
+   * @param obj object to create/add context for
+   * @param newContext context values
+   */
+  public addContext(obj: object, newContext: string[]|string) {
+    let context = ContextService.normalizeStringArguments(newContext);
 
-    if (!this.contextMap.has(obj)) {
+    if (this.contextMap.has(obj)) {
       context = [...this.contextMap.get(obj), ...context];
     }
 
