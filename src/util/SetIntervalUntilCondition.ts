64bab861060da8e253bdb18c3bc224bd8fb99479
mika.zimina <mika.zimina@centro.team>
2019-09-21 07:38:00 +0300

    Add helper

---
+++ src/util/SetIntervalUntilCondition.ts
@@ -0,0 +1,24 @@
+export default (
+  repeatFunction: () => any,
+  exitCondition: () => boolean,
+  interval: number,
+  timeout: number,
+  timeoutMessage: string = 'Promise timeout',
+): any =>
+  new Promise((res, rej) => {
+    const die = setTimeout(
+      () => rej(new Error(timeoutMessage)),
+      timeout,
+    );
+    const execute = setInterval(
+      () => {
+        const result = repeatFunction();
+        if (exitCondition()) {
+          clearTimeout(die);
+          clearInterval(execute);
+          res(result);
+        }
+      },
+      interval,
+    );
+  });
